da708d9dcd62e117aba8b0e10118fdefee410fc8
Agathatran <agathatran91.gmail.com>
2025-08-15 00:22:15 +0900

    Fix page QLVatTu

--- frontend/phongmach/src/App.tsx
+++ frontend/phongmach/src/App.tsx
@@ -42,42 +42,44 @@ import EditThuoc from './pages/Thuoc/EditThuoc';
 // import CreateBN from './pages/BenhNhan/CreateBN';
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile/searchpass1" element={<SearchPass1 />} />
-        <Route path="/profile/searchpass2" element={<SearchPass2 />} />
-        <Route path="/profile/searchpass3" element={<SearchPass3 />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/changepassword" element={<ChangePassword />} />
-        <Route path ="/editprofile" element ={<EditProfile/>} />
-        <Route path="/qlgkb" element={<QLGKB />} />
-        <Route path="/qlgkb/detail" element={<DetailGKB />} />
-        <Route path="/qlgkb/print" element={<PrintGKB />} />
-        <Route path="/tao-benh-nhan" element={<TaoBenhNhan />} />
-        <Route path="/danh-sach-benh-nhan" element={<DanhSachBenhNhan/>} />
-        <Route path="/xoa-benh-nhan/:id" element={<XoaBenhNhan/>} />
-        <Route path="/danh-sach-benh-nhan/edit/:id" element={<EditBenhNhan />} />
-        <Route path="/qldonthuoc" element={<QLDonThuoc />} />
-        <Route path="/qlvattu" element={<QLVatTu />} />
-        <Route path="/caidat" element={<CaiDat />} />
-        <Route path="/qldonthuoc/tao" element={<CreateDT />} />
-        <Route path="/thuoc" element={<QLThuoc />} />
-        <Route path="/qlkho/qlxuatnhap" element={<QLXuatNhap />} />
-        <Route path="/hotro" element={<Hotro />} />
-        <Route path="/caidat/taikhoan" element={<CaiDatTaiKhoan />} />
-        <Route path="/caidat/vaitro" element={<CaiDatVaiTro />} />
-        <Route path="/thuoc/edit/:code" element={<EditDonThuoc />} />
-        <Route path="/qlkho/addphieu" element={<AddPhieu />} />
-        <Route path="/qlgkb/edit" element={<EditGKB />} />
-        <Route path="/QLThuoc/themthuoc" element={<AddDrugForm/>} />
-        <Route path="danh-sach-thuoc" element={<DanhSachThuoc />} />
-        <Route path="thuoc/edit/:code" element={<EditThuoc />} />
-      </Routes>
-    </Router>
+    <VatTuProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/searchpass1" element={<SearchPass1 />} />
+          <Route path="/profile/searchpass2" element={<SearchPass2 />} />
+          <Route path="/profile/searchpass3" element={<SearchPass3 />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/changepassword" element={<ChangePassword />} />
+          <Route path ="/editprofile" element ={<EditProfile/>} />
+          <Route path="/qlgkb" element={<QLGKB />} />
+          <Route path="/qlgkb/detail" element={<DetailGKB />} />
+          <Route path="/qlgkb/print" element={<PrintGKB />} />
+          <Route path="/tao-benh-nhan" element={<TaoBenhNhan />} />
+          <Route path="/danh-sach-benh-nhan" element={<DanhSachBenhNhan/>} />
+          <Route path="/xoa-benh-nhan/:id" element={<XoaBenhNhan/>} />
+          <Route path="/danh-sach-benh-nhan/edit/:id" element={<EditBenhNhan />} />
+          <Route path="/qldonthuoc" element={<QLDonThuoc />} />
+          <Route path="/qlvattu" element={<QLVatTu />} />
+          <Route path="/caidat" element={<CaiDat />} />
+          <Route path="/qldonthuoc/tao" element={<CreateDT />} />
+          <Route path="/thuoc" element={<QLThuoc />} />
+          <Route path="/qlkho/qlxuatnhap" element={<QLXuatNhap />} />
+          <Route path="/hotro" element={<Hotro />} />
+          <Route path="/caidat/taikhoan" element={<CaiDatTaiKhoan />} />
+          <Route path="/caidat/vaitro" element={<CaiDatVaiTro />} />
+          <Route path="/thuoc/edit/:code" element={<EditDonThuoc />} />
+          <Route path="/qlkho/addphieu" element={<AddPhieu />} />
+          <Route path="/qlgkb/edit" element={<EditGKB />} />
+          <Route path="/QLThuoc/themthuoc" element={<AddDrugForm/>} />
+          <Route path="danh-sach-thuoc" element={<DanhSachThuoc />} />
+          <Route path="thuoc/edit/:code" element={<EditThuoc />} />
+        </Routes>
+      </Router>
+    </VatTuProvider>
   )
 }
 export default App;
